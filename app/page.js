351c564920ac1a0423461670c259375aff0f29a1
Carlos Lugones <contact@carloslugones.com>
2025-03-02 06:23:47 +0100

    perf: social links

--- app/page.js
+++ app/page.js
@@ -151,10 +151,10 @@ export default function Home() {
           <a href="https://github.com/CarlosLugones/private-chat-frontend" target="_blank">
             Code
           </a>
-          <a href="https://github.com/CarlosLugones/private-chat-frontend" target="_blank">
+          <a href="https://t.me/PrivateChatUpdates" target="_blank">
             News
           </a>
-          <a href="https://blog.carloslugones.com/private-chat" target="_blank">
+          <a href="https://teletype.in/@privatechat" target="_blank">
             Blog
           </a>
         </div>
